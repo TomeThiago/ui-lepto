c86eeb44b2d0365eea8732410b00bebe21b85b1e
Thiago Tomé <thiagotome10_@hotmail.com>
2020-07-22 13:59:06 -0300

    first commit

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  colorSecondary: '#0272cc',
+
+  appBg: '#0b0a0d',
+  appContentBg: '#16161A',
+  appBorderColor: '#0272cc',
+
+  textColor: '#fff',
+
+  // Toolbar default and active colors
+  barTextColor: '#E6E6E6',
+  barSelectedColor: '#ffffff',
+  barBg: '#1b1a1f',
+
+  brandTitle: 'Lepto Tech',
+  brandUrl: 'https://google.com.br/?q=leptotech',
+});
